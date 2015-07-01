1c9f555d968160bc19519a8481f63be116dd98f7
Mike Solomon <msolomon@thrillist.com>
2015-07-01 12:23:27 -0400

    initial commit

---
+++ hipchat.rb
@@ -0,0 +1,111 @@
+require 'sinatra'
+require 'hipchat'
+require 'rest-client'
+require 'json'
+require './config.rb'
+
+client = HipChat::Client.new(HIPCHAT_API_TOKEN, :api_version => 'v2')
+
+get '/test-post' do
+	# 'username' is the name for which the message will be presented as from
+	#client['miketest'].send('@neptune', 'I talk')
+
+	# Send notifications to users (default false)
+	#client['my room'].send('username', 'I quit!', :notify => true)
+
+	# Color it red. or "yellow", "green", "purple", "random" (default "yellow")
+	#client['my room'].send('username', 'Build failed!', :color => 'red')
+
+	# Have your message rendered as text in HipChat (see https://www.hipchat.com/docs/apiv2/method/send_room_notification)
+	client['Commerce'].send('@neptune', '<table><th>Image</th><tr><td><img src="http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2015/4/24/1429874666489/168b8f53-fa5c-4798-a571-6d614875509e-1020x612.png"/></td></tr></table>', :message_format => 'html', :notify => true, :color => 'random')
+end
+
+get '/' do
+	hello
+end
+
+get '/set_subscription' do
+	puts params.inspect
+	response = params["hub.challenge"]
+	puts response
+	response
+end
+
+post '/set_subscription' do
+	push = JSON.parse(request.body.read)
+	puts push
+
+	media_id = push[0]["data"]["media_id"]
+	puts "media id"
+	puts media_id
+
+	url = "https://api.instagram.com/v1/media/" + media_id
+	puts "url"
+	puts url
+
+	response = JSON.parse(RestClient.get(url, {:params => {:access_token => INSTAGRAM_ACESS_TOKEN_NEPTUNE}, :accept => :json} ) )
+	img_url = response["data"]["images"]["standard_resolution"]["url"]
+	caption = response["data"]["caption"]["text"]
+	link = response["data"]["link"]
+	puts img_url
+
+	client['Commerce'].send("@neptune", "<table><th>#{caption}</th><tr><td><a href='#{link}' target='_blank'><img src='#{img_url}'/></a></td></tr></table>", :message_format => 'html', :notify => true, :color => 'random')
+
+end
+
+get '/authorize' do
+	puts INSTAGRAM_AUTH_URL
+	redirect INSTAGRAM_AUTH_URL
+end
+
+get '/auth-redirect' do
+	access_token = request.fullpath.split('#access_token=')[1]
+	if access_token == nil
+		puts params.inspect
+		code = params["code"]
+
+		system "curl -F '#{INSTAGRAM_CLIENT_ID}' \
+			-F 'client_secret=#{INSTAGRAM_CLIENT_SECRET}' \
+			-F 'grant_type=authorization_code' \
+			-F 'redirect_uri=#{INSTAGRAM_OAUTH_REDIRECT_URI}' \
+			-F 'code=#{code}' \
+			https://api.instagram.com/oauth/access_token"
+	else
+		puts access_token
+	end
+
+=begin
+	data = {
+		"client_id" => INSTAGRAM_CLIENT_ID,
+		"client_secret" => INSTAGRAM_CLIENT_SECRET,
+		"grant_type" => "authorization_code",
+		"redirect_uri" => INSTAGRAM_OAUTH_REDIRECT_URI,
+		"code" => code
+	}.to_json
+
+	puts data
+
+	headers = {
+		:"Content-Type" => "application/json"
+	}
+
+	response = RestClient.post( "http://api.instagram.com/oauth/access_token", data, headers)
+	puts response
+=end
+end
+
+get '/easy-auth' do
+	access_token = request.fullpath.split('#access_token=')[1]
+	if access_token == nil
+		instagram_auth = "https://instagram.com/oauth/authorize/?client_id="+INSTAGRAM_CLIENT_ID+"&redirect_uri="+INSTAGRAM_EASY_AUTH_REDIRECT_URI+"&response_type=token"
+		puts instagram_auth
+		redirect instagram_auth
+	else
+		puts access_token
+	end
+end
+
+get '/easy-auth/redirect' do
+	access_token = request.fullpath.split('#')[1]
+	puts access_token
+end
